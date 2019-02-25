644001510ecc4f486109e39cc8469d6e5a736403
Huit Carlos <emox2544@gmail.com>
2019-02-25 13:56:19 -0600

    card letter

--- lib/ui/reading_course/alphabet.dart
+++ lib/ui/reading_course/alphabet.dart
@@ -3,111 +3,139 @@ import 'package:flutter/material.dart';
 Widget alphabet(BuildContext context) {
 
   final size = MediaQuery.of(context).size;
+  final letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p',
+  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
   return Container(
 
-    alignment: Alignment.center,
-
-    child: SingleChildScrollView(
-      physics: BouncingScrollPhysics(),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-
-          SizedBox(height: 15.0),          
-          itemLetter(size: size, letter: 'a'),
-          itemLetter(size: size, letter: 'b'),
-          itemLetter(size: size, letter: 'c'),
-          itemLetter(size: size, letter: 'd'),
-          itemLetter(size: size, letter: 'e'),
-          itemLetter(size: size, letter: 'f'),
-          itemLetter(size: size, letter: 'g'),
-          itemLetter(size: size, letter: 'h'),
-          itemLetter(size: size, letter: 'i'),
-          itemLetter(size: size, letter: 'j'),
-          itemLetter(size: size, letter: 'k'),
-          itemLetter(size: size, letter: 'l'),
-          itemLetter(size: size, letter: 'm'),
-          itemLetter(size: size, letter: 'n'),
-          itemLetter(size: size, letter: 'ñ'),
-          itemLetter(size: size, letter: 'o'),
-          itemLetter(size: size, letter: 'p'),
-          itemLetter(size: size, letter: 'q'),
-          itemLetter(size: size, letter: 'r'),
-          itemLetter(size: size, letter: 's'),
-          itemLetter(size: size, letter: 't'),
-          itemLetter(size: size, letter: 'u'),
-          itemLetter(size: size, letter: 'v'),
-          itemLetter(size: size, letter: 'w'),
-          itemLetter(size: size, letter: 'x'),
-          itemLetter(size: size, letter: 'y'),
-          itemLetter(size: size, letter: 'z'),
-          SizedBox(height: 15.0)
-
-        ],
-      )
+    child: ListView.builder(
+      padding:     EdgeInsets.symmetric(vertical: 5.0, horizontal: 2.0),
+      physics:     BouncingScrollPhysics(),
+      shrinkWrap:  true,
+      itemCount:   letters.length,
+      itemBuilder: (context, index) {
+        return itemCardLetter(size: size, letter: letters[index]);
+      },
     ),
-
   );
+
 
 }
 
-Widget itemLetter({@required Size size, @required String letter}) {
+
+
+Widget itemCardLetter({@required Size size, @required String letter}) {
+
+
+  final Radius radius4 = Radius.circular(4.0);
+  final BorderRadius radiusLeft =  BorderRadius.only(topLeft: radius4, bottomLeft: radius4);
+  final BorderRadius radiusRight = BorderRadius.only(topRight: radius4, bottomRight: radius4);
+
+  final letterItem = (String letter) {
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 12.0),
+      child: Text(
+        letter,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 68.0
+        ),
+      ),
+    );
+
+  };
+
+
+
   return Container(
 
-    height: 100.0,
-    child: Card(
+    height: 108.0,
+    child:  Card(
+
+
       child: Row(
         children: <Widget>[
-          Container(
-            // color: Colors.lime,
-            height: size.height,
-            width: 60.0,
-            alignment: Alignment.center,
 
-            child: Container(
-              width: 40.0,
-              height: 40.0,
-              margin: EdgeInsets.all(0),
-              child: RaisedButton(
-                
-                color: Colors.green,
-                child: Icon(Icons.volume_up, color: Colors.white, size: 36.0,),
-                onPressed: () {},
-              ),
-            ),
 
-          ),
+
           Expanded(
+
             child: Container(
-              height: 100.0,
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              // color: Colors.pink,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              decoration: BoxDecoration( borderRadius: radiusLeft ),
+              child: Stack(
                 children: <Widget>[
-                  Text(letter.toUpperCase(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 64.0)),
-                  SizedBox(width: 20.0),
-                  Text(letter.toLowerCase(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 64.0)),
+
+
+                  Container(
+                    height:     100.0,
+                    decoration: BoxDecoration( borderRadius: radiusLeft ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        letterItem(letter.toUpperCase()),
+                        letterItem(letter.toLowerCase())
+                      ],
+                    ),
+                  ),
+
+
+                  Container(
+                    height:     100.0,
+                    width:      60.0,
+                    decoration: BoxDecoration( borderRadius: radiusLeft ),
+
+                    child: Material(
+
+                      color:        Colors.transparent,
+                      borderRadius: radiusLeft,
+                      child:        InkWell(
+
+                        splashColor:    Colors.orange[100],
+                        highlightColor: Colors.transparent,
+                        child: Icon( Icons.play_circle_outline, size:  42.0, color: Colors.orangeAccent ),
+                        onTap: () => print('letter: $letter'),
+
+                      ),
+
+                    )
+                  ),
+
+
                 ],
               ),
             ),
+
           ),
+
+
+
           Container(
-            width:  98.0,
-            height: 98.0,
-            color:  Colors.black87,
-            child: Icon(Icons.image, color: Colors.white,),
+
+            width:      100.0,
+            padding:    EdgeInsets.all(10.0),
+            decoration: BoxDecoration( color: Colors.black87, borderRadius: radiusRight ),
+            child:      Image( image: AssetImage('assets/user-min.png') ),
+
           )
+
+
+
         ],
       ),
+
+
     ),
 
   );
+
+
 }
 
+
+
 Widget tabAlphabet() {
+
   return Tab(
 
     child: Row(
@@ -127,4 +155,5 @@ Widget tabAlphabet() {
     )
 
   );
+
 }
