87fc892d981934ad0bf1d2a4d9e7b41788aa7517
Huit Carlos <emox2544@gmail.com>
2019-02-28 11:33:50 -0600

    custom cobinations buttons

--- lib/ui/reading_course/learned_letters.dart
+++ lib/ui/reading_course/learned_letters.dart
@@ -8,31 +8,11 @@ Widget learnedLetters( BuildContext context, ReadingCourseViewModel viewModel ) 
 
 
   if (viewModel.isLoading == true) {
-    return Container(
-      padding: EdgeInsets.only(top: 20.0),
-      alignment: Alignment.topCenter,
-      child: CircularProgressIndicator(),
-    );
+    return loadingIndicator();
   }
 
   if (viewModel.isLoading == false) {
-    return Container(
-
-      padding: EdgeInsets.all(10.0),
-      child: ListView.builder(
-        
-        shrinkWrap:  true,
-        physics:     BouncingScrollPhysics(),
-        itemCount:   viewModel.learnedLetters.length,
-        itemBuilder: (BuildContext context, int i) {
-
-          return itemLearnedLetter( learnedLetter: viewModel.learnedLetters[i], viewModel: viewModel);
-
-        },
-
-      )
-
-    );
+    return learnedLettersView(viewModel);
 
   }
 
@@ -40,15 +20,51 @@ Widget learnedLetters( BuildContext context, ReadingCourseViewModel viewModel ) 
 
 }
 
+Widget learnedLettersView(ReadingCourseViewModel vm) {
+  return Container(
+
+    padding: EdgeInsets.all(10.0),
+    child:   ListView.builder(
+      
+      shrinkWrap:  true,
+      physics:     BouncingScrollPhysics(),
+      itemCount:   vm.learnedLetters.length,
+      itemBuilder: (BuildContext context, int i) {
+
+        return itemLearnedLetter( learnedLetter: vm.learnedLetters[i], viewModel: vm);
+
+      },
+
+    )
+
+  );
+}
+
+
+
+
+Widget loadingIndicator() {
+  return Container(
+    padding:   EdgeInsets.only(top: 20.0),
+    alignment: Alignment.topCenter,
+    child:     CircularProgressIndicator(),
+  );
+}
+
+
+
+
 Widget itemLearnedLetter({@required LearnedLetter learnedLetter, @required ReadingCourseViewModel viewModel}) {
 
   return Card(
+
     elevation: 1.0,
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(0))),
-    margin: EdgeInsets.symmetric(vertical: 4.0),
+    shape:     RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(0))),
+    margin:    EdgeInsets.symmetric(vertical: 4.0),
     child: ExpansionTile(
 
-      title: Row(
+      leading: Icon(Icons.favorite, color: Colors.red,),
+      title:   Row(
 
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
@@ -75,6 +91,7 @@ Widget itemLearnedLetter({@required LearnedLetter learnedLetter, @required Readi
               ],
             ),
           ),
+
           Container(
             margin: EdgeInsets.symmetric(vertical: 20.0),
             child: Row(
@@ -88,89 +105,79 @@ Widget itemLearnedLetter({@required LearnedLetter learnedLetter, @required Readi
             ),
           ),
 
-
-
         ],
 
       ),
-      leading: Icon(Icons.favorite, color: Colors.red,),
+
+
       children: <Widget>[
 
+
         Container(
-          margin: EdgeInsets.symmetric(vertical: 10.0),
-          child: Wrap(
-            direction: Axis.horizontal,
-            crossAxisAlignment: WrapCrossAlignment.center,
-            alignment: WrapAlignment.center,
+          margin:  EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+          padding: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color:  Colors.grey[100],
+            border: Border.all(width: 1.0, color: Colors.grey[300]),
+            borderRadius: BorderRadius.all(Radius.circular(3.0))
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                height: 45.0,
-                width: 120.0,
-                child: RaisedButton(
-                  color: Colors.green,
-                  textColor: Colors.white,
-                  onPressed: () => viewModel.speak(term: '${learnedLetter.letter} mayúscula', rate: 1.0),
-                  child: Text(
-                    learnedLetter.letter.toUpperCase(),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 28.0
-                    ),
-                  ),
-                )
-              ),
-              SizedBox(width: 20.0,),
-              Container(
-                height: 45.0,
-                width: 120.0,
-                child: RaisedButton(
-                  color: Colors.green,
-                  textColor: Colors.white,
-                  onPressed: () => viewModel.speak(
-                    term: '${learnedLetter.letter} minúscula',
-                    rate: 1.0
-                  ),
-                  child: Text(learnedLetter.letter,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28.0
-                  ),
-                ),
-                )
-              )
+
+              individualLetterBtn(letter: learnedLetter.letter.toUpperCase(), typeLetter: 'mayúscula', vm: viewModel),
+              individualLetterBtn(letter: learnedLetter.letter.toLowerCase(), typeLetter: 'minúscula', vm: viewModel),
+
             ],
           ),
         ),
 
+
         Container(
+          margin:  EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+          padding: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color:        Colors.grey[100],
+            border:       Border.all(width: 1.0, color: Colors.grey[300]),
+            borderRadius: BorderRadius.all(Radius.circular(3.0))
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              Row(),
+              Wrap(
 
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: Wrap(
+                alignment: WrapAlignment.center,
+                spacing:    7.0,
+                runSpacing: 10.0,
+                children: List.generate(
+                  learnedLetter.combinations.length,
+                  (i) => Container(
+                    width: 90.0,
+                    height: 40.0,
+                    child: RaisedButton(
+                      color:     Colors.red,
+                      textColor: Colors.white,
+                      highlightElevation: 4.0,
+                      onPressed: () => viewModel.speak(term: learnedLetter.combinations[i].p),
+                      child: Text(
 
-            direction: Axis.horizontal,
-            crossAxisAlignment: WrapCrossAlignment.center,
-            alignment: WrapAlignment.center,
-            spacing: 5.0,
-            children:List.generate(learnedLetter.combinations.length, (i) => RaisedButton(
+                        learnedLetter.combinations[i].w,
 
-              color:     Colors.red,
-              textColor: Colors.white,
-              highlightElevation: 4.0,
-              onPressed: () => viewModel.speak(term: learnedLetter.combinations[i].p),
-              child: Text(
-
-                learnedLetter.combinations[i].w,
-
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.0,
-                  fontSize: 24.0
-                ),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.0,
+                          fontSize: 24.0
+                        ),
+                      ),
+                    )
+                  )
+                )
               ),
-            ))
-
+            ],
           ),
         ),
+
 
         Container(
           margin: EdgeInsets.only(top: 20.0, bottom: 30.0),
@@ -178,25 +185,52 @@ Widget itemLearnedLetter({@required LearnedLetter learnedLetter, @required Readi
             width: 225.0,
             height: 40.0,
             child: RaisedButton(
-              color: Colors.orange,
+              highlightElevation: 6.0,
+              color: Colors.green,
               textColor: Colors.white,
               child: Text(
                 'Volver a praticar',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 19.0
-                ),
+                style: TextStyle( fontWeight: FontWeight.bold, fontSize: 19.0, letterSpacing: 0.8 ),
               ),
               onPressed: () {},
             ),
           )
         )
 
+
       ],
     )
   );
 
 }
+
+
+
+
+/// [Widget] return a button with a individual Letter 
+Widget individualLetterBtn({
+  @required String letter,
+  @required String typeLetter,
+  @required ReadingCourseViewModel vm
+}) {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 5.0),
+    width:  120.0,
+    height: 50.0,
+    child: RaisedButton(
+      highlightElevation: 4.0,
+      color:     Colors.lightBlue,
+      textColor: Colors.white,
+      onPressed: () => vm.speak(term: '$letter $typeLetter', rate: 1.0),
+      child:     Text(
+        letter,
+        style: TextStyle( fontWeight: FontWeight.bold, fontSize: 32.0 ),
+      ),
+    ),
+  );
+}
+
+
 
 
 Widget tabLearnedLetters() {
@@ -220,46 +254,3 @@ Widget tabLearnedLetters() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-
-    return Container(
-      child: ListView.builder(
-        itemCount: viewModel.learnedLetters.length,
-        itemBuilder: (context, index) {
-          return ExpansionTile(
-            title: Row(
-              children: <Widget>[
-                Text('A', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),),
-                Text('a', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),),
-              ],
-            ),
-            // backgroundColor: Colors.red,
-
-            leading: Icon(Icons.ac_unit),
-            children: <Widget>[
-              Container(
-                color: Colors.red,
-                child: Text('12345'),
-              )
-            ],
-          );
-        },
-      ),
-
-    );
-
- */
