618ba86e01bc483a6defe77355a39ce471fc63eb
Huit Carlos <emox2544@gmail.com>
2019-04-12 15:10:57 -0600

    compatibilty with androidX

--- lib/ui/reading_course/pronounce_letters/pronounce_letters_screen.dart
+++ lib/ui/reading_course/pronounce_letters/pronounce_letters_screen.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+// import 'package:permission_handler/permission_handler.dart';
 
-class PronounceLettersScreen extends StatelessWidget {
+class PronounceLettersScreen extends StatefulWidget {
+  @override
+  _PronounceLettersScreenState createState() => _PronounceLettersScreenState();
+}
+
+class _PronounceLettersScreenState extends State<PronounceLettersScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    requestePermision();
+  }
+
+  Future<void> requestePermision() async {
+    // await PermissionHandler().requestPermissions([
+    //   PermissionGroup.microphone,
+    //   PermissionGroup.speech,
+    // ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
