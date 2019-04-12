757cfffac28cb243c2f0d6b1796883441fdba026
Huit Carlos <emox2544@gmail.com>
2019-04-12 09:42:18 -0600

    stop audio on dispose

--- lib/ui/reading_course/select_words/select_words_screen.dart
+++ lib/ui/reading_course/select_words/select_words_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app19022019/core/src/redux/app/app_state.dart';
 import 'package:app19022019/core/src/redux/reading_course/rc_select_words/rc_select_words_actions.dart';
+import 'package:app19022019/core/src/services/speech_synthesis_service.dart';
 import 'package:app19022019/core/src/viewmodels/reading_course/select_words_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
@@ -18,7 +19,10 @@ class _SelectWordsScreenState extends State<SelectWordsScreen> {
       distinct:  true,
       converter: (store) => SelectWordsViewModel.fromStore(store),
       onInit:    (store) => store.dispatch(RCSetInitialDataSW()),
-      onDispose: (store) => store.dispatch(RCResetDataSW()),
+      onDispose: (store) {
+        store.dispatch(RCResetDataSW());
+        SpeechSynthesisService.stop();
+      },
       builder:   (_, vm) => SelectWordsContent(viewModel: vm)
 
     );
