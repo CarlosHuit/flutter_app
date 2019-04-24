e5cec0841027e4f5f15d35ae10bb0c2df36d10d9
Huit Carlos <emox2544@gmail.com>
2019-04-23 20:28:42 -0600

    code refactorization - cancel tts on pop scope

---
+++ lib/ui/reading_course/letter_detail/letter_detail_content.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+import 'package:app19022019/core/src/viewmodels/reading_course/letter_detail_view_model.dart';
+import './bottom_sheet_letter_detail.dart';
+import './option_card.dart';
+import './try_again_dialog.dart';
+import './well_done_dialog.dart';
+
+class LetterDetailContent extends StatefulWidget {
+
+  final LetterDetailViewModel vm;
+
+  const LetterDetailContent({ Key key, @required this.vm }) : super(key: key);
+
+  @override
+  _LetterDetailContentState createState() => _LetterDetailContentState();
+}
+
+class _LetterDetailContentState extends State<LetterDetailContent> {
+
+
+  LetterDetailViewModel get vm => widget.vm;
+
+  @override
+  void initState() {
+
+    super.initState();
+    showModalSheet(vm, context);
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size    = MediaQuery.of(context).size;
+    final width90 = size.width * 0.90;
+    final options = List.generate(
+      widget.vm.options.length,
+      (i) => OptionCard(
+        vm: widget.vm,
+        letterId: '${widget.vm.options[i]}$i',
+        hideAllCars: widget.vm.hideAllCards,
+        showAllCards: widget.vm.showAllCards,
+      )
+    );
+
+    return Scaffold(
+      body: Stack(
+
+        children: <Widget>[
+
+          Container(
+
+            child: Container(
+              alignment:  Alignment.center,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit:   BoxFit.fitHeight,
+                  image: AssetImage("assets/star-pattern.png"),
+                ),
+                gradient: LinearGradient(
+                  stops:    [0.0, 0.8],
+                  begin: Alignment.bottomLeft,
+                  end:   Alignment.topRight,
+                  colors: [
+                    Color.fromARGB(255, 11, 62, 113),
+                    Color.fromARGB(255, 13, 100, 176)
+                  ]
+                )
+              ),
+
+              child: Container(
+                width:  width90,
+                height: (width90 / 3) * 4,
+                child:  GridView.count(
+
+                  crossAxisCount: 3,
+                  shrinkWrap: true,
+                  physics:    NeverScrollableScrollPhysics(),
+                  padding:    EdgeInsets.all(1.0),
+                  children:   options,
+
+                ),
+              ),
+
+            )
+
+          ),
+
+          Positioned(
+            child: vm.showWellDoneDialog
+              ? WellDoneDialog(
+                viewModel:  vm,
+                speak:      vm.listenCorrectMsg,
+                hideDialog: vm.hideWellDoneDialog,
+                canShowModalSheet: vm.currentIndex < vm.dataLength -1 ? true : false,
+                callBack: () => print('Change Current Data'),
+              )
+              : SizedBox()
+          ),
+
+          Positioned(
+            child: vm.showTryAgainDialog
+              ? TryAgainDialog(
+                speak:      vm.listenIncorrectMsg,
+                hideDialog: vm.hideTryAgainDialog,
+                callBack: () => print('CallBack executed'),
+              )
+              : SizedBox(),
+          ),
+
+        ]
+      ),
+    );
+
+  }
+
+
+}
--- lib/ui/reading_course/letter_detail/letter_detail_screen.dart
+++ lib/ui/reading_course/letter_detail/letter_detail_screen.dart
@@ -1,16 +1,12 @@
 import 'package:app19022019/core/src/redux/app/app_state.dart';
 import 'package:app19022019/core/src/redux/reading_course/rc_letter_detail/rc_letter_detail_actions.dart';
+import 'package:app19022019/core/src/services/speech_synthesis_service.dart';
 import 'package:app19022019/core/src/viewmodels/reading_course/letter_detail_view_model.dart';
-import 'package:app19022019/ui/reading_course/letter_detail/letter_detail_body.dart';
+import 'package:app19022019/ui/reading_course/letter_detail/letter_detail_content.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
-class LetterDetailScreen extends StatefulWidget {
-  @override
-  _LetterDetailScreenState createState() => _LetterDetailScreenState();
-}
-
-class _LetterDetailScreenState extends State<LetterDetailScreen> {
+class LetterDetailScreen extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
@@ -20,11 +16,18 @@ class _LetterDetailScreenState extends State<LetterDetailScreen> {
       onInit:    (store) => store.dispatch(RCSetInitialDataLD()),
       onDispose: (store) => store.dispatch(RCResetDataLD()),
       converter: (store) => LetterDetailViewModel.fromStore(store),
-      builder:   (_, vm) => Scaffold( body: LetterDetailBody(vm: vm) )
+      builder:   (_, vm) => WillPopScope(
+        onWillPop: cancelTts,
+        child: LetterDetailContent(vm: vm)
+      )
 
     );
 
   }
 
+  Future<bool> cancelTts() async{
+    SpeechSynthesisService.stop();
+    return true;
+  }
 
 }
