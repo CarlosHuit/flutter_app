949bec15573a6682b133cd47eba7d84e68db9beb
Huit Carlos <emox2544@gmail.com>
2019-05-24 15:36:37 -0600

    remove invalid imports

--- lib/core/src/models/models.dart
+++ lib/core/src/models/models.dart
@@ -2,5 +2,4 @@ export './account_form.dart';
 export './auth_login_response.dart';
 export './course_model.dart';
 export './credentials.dart';
-export './login_response.dart';
 export './user_model.dart';
